6eabcbd1f684f491fd7c0c2011cae9d51dfae739
insane013 <marsel092002@gmail.com>
2020-11-30 17:50:49 +0500

    added header-top section & part of header-content

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var headerTop_slider = new Swiper('.header-content__slider', {
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+})
