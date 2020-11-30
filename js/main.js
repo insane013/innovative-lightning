1f4b4e796baad1e583e1a5ec26996663357daf18
insane013 <marsel092002@gmail.com>
2020-11-30 19:49:44 +0500

    added some decorations

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ var headerTop_slider = new Swiper('.header-content__slider', {
 
   // Navigation arrows
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: '.header-content__arrow header-content__arrow--next',
+    prevEl: '.header-content__arrow header-content__arrow--prev',
   },
 })
