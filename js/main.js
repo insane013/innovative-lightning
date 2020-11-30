82a8d55725822286130a0080e24b943d1b9d12b3
insane013 <marsel092002@gmail.com>
2020-11-30 20:51:24 +0500

    enabled slider

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
-var headerTop_slider = new Swiper('.header-content__slider', {
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.header-content__arrow header-content__arrow--next',
-    prevEl: '.header-content__arrow header-content__arrow--prev',
-  },
-})
+$(document).ready(function () {
+  var headerTopSlider = new Swiper('.header-content__slider', {
+    loop: true,
+    speed: 1000,
+    navigation: {
+      nextEl: '.header-content__arrow--next',
+      prevEl: '.header-content__arrow--prev',
+    },
+  })
+});
