be7fb467229afee57fba38916f267352f5fd4d33
insane013 <marsel092002@gmail.com>
2020-12-01 13:20:04 +0500

    part of adaptive

--- js/main.js
+++ js/main.js
@@ -7,4 +7,20 @@ $(document).ready(function () {
       prevEl: '.header-content__arrow--prev',
     },
   })
+
+  $("[data-interact='dropdown']").on('click', function(e){
+    e.preventDefault,
+    $($(this).attr("data-href")).toggleClass("dropdown-content--show")
+  });
+
+  $("[data-interact='toggle-menu']").on('click', function(e){
+    e.preventDefault,
+    $($(this).attr("data-href")).toggleClass("menu--mobile--visible")
+    $(this).toggleClass("menu-btn--header--menu-open")
+  });
+  $("[data-interact='close-menu']").on('click', function(e){
+    e.preventDefault,
+    $($(this).attr("data-href")).removeClass("menu--mobile--visible")
+  })
+
 });
