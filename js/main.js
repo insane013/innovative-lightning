a84cf17bb49c1e86a61559aea875a46d276dfaa6
Insane013 <marsel092002@gmail.com>
2020-12-04 17:41:37 +0500

    fixed BG, added product-sl and coop-sl

--- js/main.js
+++ js/main.js
@@ -8,6 +8,48 @@ $(document).ready(function () {
     },
   })
 
+  var cooperateSlider = new Swiper('.we-cooperate__slider', {
+    speed: 1000,
+    // loop: true,
+    // watchOverflow: true,
+    autoHeight: true,
+    slidesPerView: 4,
+
+    navigation: {
+      nextEl: '.we-cooperate__arrow--next',
+      prevEl: '.we-cooperate__arrow--prev',
+    },
+
+    breakpoints: {
+      320:{
+        slidesPerView: 1
+      },
+      700:{
+        slidesPerView: 3,
+      },
+      992:{
+        slidesPerView: 4
+      },
+      1200:{
+        spaceBetween: 113,
+      }
+    }
+  })
+
+  var produceSlider = new Swiper('.we-produce__slider', {
+    speed: 1000,
+    // loop: true,
+    // watchOverflow: true,
+    autoHeight: true,
+    slidesPerView: 4,
+
+    navigation: {
+      nextEl: ',we-produce__arrow--next',
+      prevEl: '.we-produce__arrow--prev',
+    },
+
+  })
+
   $("[data-interact='dropdown']").on('click', function(e){
     e.preventDefault,
     $($(this).attr("data-href")).toggleClass("dropdown-content--show")
